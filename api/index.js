0b0b969b2e1558a679e3c651499b51d7f81c50ca
Jennifer Nguyen <jnhallquist@gmail.com>
2017-12-12 16:43:02 -0800

    Create commando bot

--- api/index.js
+++ api/index.js
@@ -1,13 +1,26 @@
-const config = require('./config.json')
 const Commando = require('discord.js-commando');
 const Client = require('pg');
+const path = require('path');
+
+const config = require('./config.json');
 
 const bot = new Commando.Client({
-    owner: '388398975516016640'
+    owner: config.owner,
+    commandPrefix: 'rbot'
 });
 
 bot.on('ready', () => {
   console.log('I am ready!');
 });
+
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+bot.registry
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
 bot.login(config.token);
