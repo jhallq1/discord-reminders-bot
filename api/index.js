3d89717747cb1774d789280bdcba5c78be47433f
Jennifer Nguyen <jnhallquist@gmail.com>
2017-12-20 14:28:18 -0800

    Connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
-
 const config = require('./config.json');
 const node_scheduler = require('./scheduler.js');
 
