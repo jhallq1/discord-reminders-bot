27c8bf0901d844c89fca9e4c2f903d6026948ddd
justin hallquist <justin.hallquist@gmail.com>
2018-01-17 20:32:49 -0800

    Cleanup

--- test/stubs/Kue.js
+++ test/stubs/Kue.js
@@ -16,29 +16,3 @@ module.exports = {
   }
 }
 
-
-// const queue = kue.createQueue({
-//   redis: {
-//     "port": keys.redisPort,
-//     "host": keys.redisHost,
-//     "auth": keys.redisKey
-//   }
-// });
-//     let job = queue.create('reminder', {
-//       target_id: target.id,
-//       content: content
-//     }).delay(millisecondsTillReminder).save(function(err) {
-//       if (!err) {
-//         return msg.direct(moment(chrono.parseDate(datetime)).calendar(
-//           moment.now(), "M/D/YYYY h:mm a") + ', ' + target +
-//           ' will be reminded "' + content + '" ');
-//       }
-//     });
-
-//     queue.process('reminder', function(job, done) {
-//       bot.fetchUser(job.data.target_id).then(user => {
-//         user.send(job.data.content);
-//       });
-
-//       done();
-//     });
