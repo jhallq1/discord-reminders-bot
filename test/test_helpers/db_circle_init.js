59c7bc0f4fe7222c74135a68e591da4bdd8a6212
justin hallquist <justin.hallquist@gmail.com>
2018-01-27 22:55:27 -0800

    Add mocha opts for loading global hook

--- test/test_helpers/db_circle_init.js
+++ test/test_helpers/db_circle_init.js
@@ -1,17 +1,28 @@
-const db = require('../../db/index.js');
-console.log(process.env['CIRCLECI']);
 if (process.env['CIRCLECI']) {
   before(() => {
+    let db      = require('../../db/index.js');
+    let fs      = require('fs');
+    let resolve = require('path').resolve;
+    let path    = resolve('db/schema.sql');
+
+    let schema = fs.readFileSync(path).toString();
+
     db.connect(err => {
       if (err) {
         console.error('DB connection failed!', err.stack);
       }
-    });
-
-    db.query('../schema.sql')
-    .then(res => {
-      console.log('DB Schema Loaded');
     })
-    .catch(err => console.error(err.stack));
+    .then(res => {
+      return db.query(schema)
+      .then(res => {
+        console.log('DB Schema Loaded');
+        db.end();
+      })
+    })
+    .catch(err => {
+      console.error(err.stack)
+      db.end();
+    });
   });
 }
+
