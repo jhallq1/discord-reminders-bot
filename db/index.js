c3cc84c13640ad7b89bc2fe289c8bd548c483c81
Jennifer Nguyen <jnhallquist@gmail.com>
2018-01-27 17:13:45 -0800

    Fix Code Climate issue

--- db/index.js
+++ db/index.js
@@ -3,41 +3,10 @@ const keys = require('./../api/keys.json');
 
 let ev = process.env.NODE_ENV = 'test' ? keys.dbDatabase_test : keys.dbDatabase;
 
-let db;
-
-module.exports = {
-  connection: () => {
-    db =  new Client({
-      host: keys.dbHost,
-      port: keys.dbPort,
-      user: keys.dbUser,
-      password: keys.dbPassword,
-      database: ev
-    });
-  },
-  connect: () => {
-    db.connect((err) => {
-      if (err) {
-        console.error('DB connection failed!', err.stack);
-      } else {
-        console.log('Connected to db');
-      }
-    });
-  }
-}
-
-
-
-// new Client({
-//   host: keys.dbHost,
-//   port: keys.dbPort,
-//   user: keys.dbUser,
-//   password: keys.dbPassword,
-//   database: ev
-// }).connect((err) => {
-//   if (err) {
-//     console.error('DB connection failed!', err.stack);
-//   } else {
-//     console.log('Connected to db');
-//   }
-// });
+module.exports = new Client({
+  host: keys.dbHost,
+  port: keys.dbPort,
+  user: keys.dbUser,
+  password: keys.dbPassword,
+  database: ev
+});
