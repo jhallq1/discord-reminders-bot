5c453eec6c62238d9b718b061976ba4e8dacd66e
justin hallquist <justin.hallquist@gmail.com>
2018-01-28 17:54:38 -0800

    Clean up pt1

--- api/util/translateDatetime.js
+++ api/util/translateDatetime.js
@@ -2,20 +2,11 @@ const chrono = require('chrono-node');
 const moment = require('moment-timezone');
 
 module.exports = (input, timezone) => {
-  let date = new Date();
-  let time_in_zone = moment(date).tz(timezone);
+  let time_in_zone = moment(new Date()).tz(timezone);
   let zone         = time_in_zone.zoneAbbr();
   let ref          = time_in_zone.format();
-
-  let parsedDate
-
-  if (timezone != 'UTC') {
-    parsedDate = chrono.parseDate(input + " " + zone, ref)
-  } else {
-    parsedDate = chrono.parseDate(input)
-  }
+  let parsedDate = chrono.parseDate(input + " " + zone, ref)
   
-
   let delayAmt = 
     parsedDate.getTime() - time_in_zone.valueOf();
 
