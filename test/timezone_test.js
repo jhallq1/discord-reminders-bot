b65cfe6d1f445135057b25eb78ce14c6242d8d39
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-04 22:37:22 -0800

    Add test for sucessful timezone insertion

--- test/timezone_test.js
+++ test/timezone_test.js
@@ -4,7 +4,7 @@ const proxyquire = require('proxyquire').noPreserveCache().noCallThru();
 const { expect } = require('chai');
 const msg        = require('./stubs/message.js');
 const exceptions = require('../api/util/exceptions.json');
-// const insertTz   = require('../db/queries/insertTimezone.js');
+const insertTz   = require('../db/queries/insertTimezone.js');
 
 /* eslint-disable global-require */
 const TimezoneCommand = proxyquire(
@@ -22,13 +22,29 @@ const subject = (message, content) => new TimezoneCommand({}).run(
 
 describe('#run', () => {
   const invalidTz = 'America/San_Diego';
+  const validTz = 'America/Los_Angeles';
+
+  const user  = {
+    id: 1,
+    username: 'test_user',
+    discriminator: '1234'
+  };
 
   describe('timezone upsertion', () => {
-    context('when timezone is not included in moment\'s zone list', () => {
+    context("when timezone is not included in moment's zone list", () => {
       it('throws invalid timezone exception', () => subject(
         msg, invalidTz
       ).catch((ex) => {
         expect(ex).to.eq(exceptions.invalid_timezone);
+      }));
+    });
+
+    context("when timezone is included in moment's zone list", () => {
+      it('upserts into table and returns rowCount of 1', () => insertTz(
+        [user.username, user.discriminator, validTz]
+      )
+      .then((res) => {
+        expect(res).to.eq(1);
       }));
     });
   });
