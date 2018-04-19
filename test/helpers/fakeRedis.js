1d45628957bb81e5d34f9b4b4cd1c1df368f3a9b
Jennifer Hallquist <jnhallquist@gmail.com>
2018-04-18 21:27:00 -0700

    Delete unused helpers

--- test/helpers/fakeRedis.js
+++
@@ -1,9 +0,0 @@
-const redis = require('fakeredis');
-const bluebird = require('bluebird');
-
-bluebird.promisifyAll(redis.RedisClient.prototype);
-
-module.exports = {
-  timezones: redis.createClient(),
-  reminders: redis.createClient()
-};
