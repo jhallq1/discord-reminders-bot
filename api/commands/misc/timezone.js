f011e3b808e9ddcf287a29af2174c080e8e64a55
Jennifer Hallquist <jnhallq@Bon-Bon.local>
2018-06-17 17:53:09 -0700

    Proxyquire redis

--- api/commands/misc/timezone.js
+++ api/commands/misc/timezone.js
@@ -46,7 +46,6 @@ module.exports = class TimezoneCommand extends Command {
 
   run(msg, { content }) {
     const tz = timezones[content - 1] || content;
-
     if (!moment.tz.names().includes(tz)) {
       return new Promise((resolve, reject) =>
         reject(msg.say(exceptions.invalid_timezone)));
--- test/timezone.js
+++ test/timezone.js
@@ -1,6 +1,6 @@
 /* global describe context it */
 
-const proxyquire = require('proxyquire');
+const proxyquire = require('proxyquire').noCallThru();
 const { expect } = require('chai');
 const msg        = require('./stubs/message.js');
 const exceptions = require('../api/util/exceptions.json');
@@ -11,7 +11,7 @@ const TimezoneCommand = proxyquire(
   {
     'discord.js-commando': require('./stubs/Command.js'),
     '../../bot.js': new (require('./stubs/CommandoClient.js').CommandoClient)(),
-    'tzStore': require('./mocks/redisClient.js').timezones
+    '../../redis/client.js': require('./mocks/redisClient.js')
   }
 );
 /* eslint-enable global-require */
@@ -20,45 +20,50 @@ const subject = (message, content) => new TimezoneCommand({}).run(
   message, { content }
 );
 
-describe('#run', () => {
-  const invalidTz = 'America/San_Diego';
-  const validTz = 'America/Los_Angeles';
+const expectedPrompt = `Quick-select your timezone by entering a number from the
+        following list. Otherwise, enter the name of your zone if it is not
+        included here.
 
-  const validInput = 6;
-  const invalidInput = 11;
+        [1] Atlantic
+        [2] Eastern
+        [3] Central
+        [4] Mountain
+        [5] MST
+        [6] Pacific
+        [7] Alaska
+        [8] Hawaii`;
 
-  describe('timezone upsertion', () => {
-    context("when timezone is not included in moment's zone list", () => {
-      it('throws invalid timezone exception', () => subject(
-        msg, 'rbot timezone'
-      ).catch((ex) => {
-        expect(ex).to.eq(exceptions.invalid_timezone);
-      }));
-    });
-
-    // context('when timezones array does not include inputted number', () => {
-    //   it('throws invalid timezone exception', () => subject(
-    //     msg, invalidInput
-    //   ).catch((ex) => {
-    //     expect(ex).to.eq(exceptions.invalid_timezone);
-    //   }));
-    // });
-
-    // context("when timezone is included in moment's zone list", () => {
-    //   it('upserts into table and DMs user success msg', () => subject(
-    //     msg, validTz
-    //   )
-    //   .then((res) => {
-    //     expect(res).to.eq('Your timezone has been set to America/Los_Angeles');
-    //   }));
-    // });
-
-    // context('when timezones array includes user inputted number', () => {
-    //   it('upserts into table and DMs user success msg', () => subject(
-    //     msg, validInput
-    //   ).then((res) => {
-    //     expect(res).to.eq('Your timezone has been set to America/Los_Angeles');
-    //   }));
-    // });
+describe('Timezone Command', () => {
+  context("when user calls 'rbot timezone'", () => {
+    it('displays list of timezones from which to select', () => subject(
+      new TimezoneCommand({})
+    ).then((res) => {
+      expect(res).to.eq(expectedPrompt);
+    }));
   });
+
+  // context('when timezones array does not include inputted number', () => {
+  //   it('throws invalid timezone exception', () => subject(
+  //     msg, invalidInput
+  //   ).catch((ex) => {
+  //     expect(ex).to.eq(exceptions.invalid_timezone);
+  //   }));
+  // });
+
+  // context("when timezone is included in moment's zone list", () => {
+  //   it('upserts into table and DMs user success msg', () => subject(
+  //     msg, validTz
+  //   )
+  //   .then((res) => {
+  //     expect(res).to.eq('Your timezone has been set to America/Los_Angeles');
+  //   }));
+  // });
+
+  // context('when timezones array includes user inputted number', () => {
+  //   it('upserts into table and DMs user success msg', () => subject(
+  //     msg, validInput
+  //   ).then((res) => {
+  //     expect(res).to.eq('Your timezone has been set to America/Los_Angeles');
+  //   }));
+  // });
 });
