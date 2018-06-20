5fad7b68462f2259e19d77369df64aa315a0c589
Jennifer Hallquist <jnhallq@Bon-Bon.local>
2018-06-19 21:40:12 -0700

    Change assertion statement from contains to have string

--- test/remind.js
+++ test/remind.js
@@ -85,7 +85,7 @@ describe('Remind Command', () => {
       it('returns successful reminder creation message', () => subject(
         msg, reminder
       ).then((res) => {
-        expect(res).to.contain(`will be reminded "This is a test reminder"`);
+        expect(res).to.have.string(`will be reminded "This is a test reminder"`);
       }));
     });
   });
